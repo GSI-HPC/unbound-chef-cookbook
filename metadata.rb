21ad62c96bc3a2d47dfb1bbed087d39f26cf91c5
C. Huhn <spoved@knollhuhn.de>
2013-09-09 13:21:05 +0200

    Fill out metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Christopher Huhn"
+maintainer_email "c.huhn@gsi.de"
 license          "GNU Public License 3.0"
-description      "Installs/Configures unbound"
+description      "Installs/Configures unbound dns resolver"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
