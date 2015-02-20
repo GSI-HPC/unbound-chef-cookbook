adcbdec9b7c163a9e292c13b9e0bda182a6daff2
Matteo Dessalvi <m.dessalvi@gsi.de>
2015-02-20 12:12:12 +0100

    Introduced DNSSEC notation in the attributes naming

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,9 @@
-default[:unbound][:insecure_domains] = [ ]
+
+# All the queries for the domains listed here will be
+# directed toward specific DNS servers.
 default[:unbound][:stub_zones]       = { }
+
+# DNSSEC attributes:
+default[:unbound][:dnssec][:enable] = false
+default[:unbound][:dnssec][:insecure_domains] = [ ]
+
