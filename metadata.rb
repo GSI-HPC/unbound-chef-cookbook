e2b2daba981f30d0963ae59f8a43502ecc0bc47b
Matteo Dessalvi <m.dessalvi@gsi.de>
2015-02-24 16:11:27 +0100

    Version bumped

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "hpc@gsi.de"
 license          "GNU Public License 3.0"
 description      "Installs/Configures unbound dns resolver"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.4"
+version          "0.0.5"
