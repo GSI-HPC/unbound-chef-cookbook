12834adfd466393542769c5c3fa2e3fc5a350827
Matteo Dessalvi <m.dessalvi@gsi.de>
2015-02-24 11:58:51 +0100

    Introduced an attribute to enable/disable caching DNS configuration

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,12 @@
 
+# By default the cookbook will configure the Unbound
+# service as a caching DNS server:
+default[:unbound][:caching] = true
+
 # All the queries for the domains listed here will be
 # directed toward specific DNS servers.
+# The stub zones will be used only when Unbound is not
+# configured as a caching DNS server.
 default[:unbound][:stub_zones]       = { }
 
 # DNSSEC attributes:
