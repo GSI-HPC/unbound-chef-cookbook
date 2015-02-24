519f9459c41a9c6721efef3dc2871f00c7e69c89
Matteo Dessalvi <m.dessalvi@gsi.de>
2015-02-24 12:10:02 +0100

    Just changed maintainer/maintainer email

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             "unbound"
-maintainer       "Christopher Huhn"
-maintainer_email "c.huhn@gsi.de"
+maintainer       "GSI HPC department"
+maintainer_email "hpc@gsi.de"
 license          "GNU Public License 3.0"
 description      "Installs/Configures unbound dns resolver"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
